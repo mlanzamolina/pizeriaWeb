60f743d0ff7d3cc2f1c30d176aa314b874be0b32
unknown <mlanzamolina@unitec.edu>
2021-09-04 18:20:39 -0600

    landing page

--- src/Pizeria.js
+++ src/Pizeria.js
@@ -2,8 +2,46 @@ import React from 'react'
 
 export default function Pizeria() {
     return (
-        <div>
-            Pizeria
-        </div>
+        <div class="container">
+             <div class="sliding-background">
+         <div class="cover">
+            <div class="nav-bar">
+
+<div class="left-bar">
+    <h1>Pizeria</h1>
+</div>
+
+<div class="right-bar">
+<ul>
+     <li>Home</li>
+ <li>Sobre Nosotros</li>
+ <li>Contactactos</li>
+ <li>Servicios</li>
+ </ul>
+
+
+</div>
+
+</div>
+
+<div class="hero-text">
+
+<h3>TRAVELLING,CAMPING,HIKING</h3>
+
+<h2>AN ESCAPE TOWARDS NORTH</h2>
+
+<h4>Come and visit pakistan with us</h4>
+
+</div>
+
+<button id="btn-1">more</button>
+
+<button id="btn-2">sign-up</button>
+
+</div>
+
+
+</div>
+</div>
     )
 }
